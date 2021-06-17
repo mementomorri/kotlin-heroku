dd0a3187f9d561ce492505c783e0d041705c0e78
mementomorri <44745297+mementomorri@users.noreply.github.com>
2021-06-17 18:31:57 +0600

    Update Application.kt

--- src/Application.kt
+++ src/Application.kt
@@ -22,7 +22,7 @@ import rest.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
+@file:Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
     install(CORS) {
